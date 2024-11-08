2373ba5439ec906d6a895331c0eefdc8583e213c
Lauri Hietanen <16720134+hielau@users.noreply.github.com>
2024-11-08 09:17:25 +0200

    changed init.lua

--- init.lua
+++ init.lua
@@ -90,8 +90,11 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netwrPlugin = 1
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -102,7 +105,11 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
+<<<<<<< HEAD
 -- vim.o.relativenumber = true
+=======
+vim.opt.relativenumber = true
+>>>>>>> 5d6d274 (changed init.lua)
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -247,7 +254,13 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+<<<<<<< HEAD
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+=======
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'hielau/vim-elmer',
+  'nvim-tree/nvim-tree.lua',
+>>>>>>> 5d6d274 (changed init.lua)
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -700,6 +713,22 @@ require('lazy').setup({
         },
       }
 
+      -- nvim-tree options
+      require('nvim-tree').setup {
+        sort = {
+          sorter = 'case_sensitive',
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      }
+
       -- Ensure the servers and tools above are installed
       --
       -- To check the current status of installed tools and/or manually install
@@ -716,6 +745,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'fortls',
+        'pylsp',
+        'clangd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
